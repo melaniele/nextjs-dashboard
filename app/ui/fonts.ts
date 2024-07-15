3e285042ff27284cdabe313843658149b00c6c70
Melanie Le <77984641+melaniele@users.noreply.github.com>
2024-07-15 10:13:03 -0400

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+// keep the fonts that will be used throughout application
+import {Inter, Lusitana} from 'next/font/google'
+
+export const inter = Inter({subsets: ['latin']})
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin']
+})
